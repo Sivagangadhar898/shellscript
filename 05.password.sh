84b28bdaff849beb36cf1760b9beefa88527daa7
siva <desinidisivagangadhar@gmail.com>
2023-11-24 12:15:43 +0530

    succsesfully done

--- 05.password.sh
+++ 05.password.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
  echo "BRO Please enter your username"
+read -s USERNAME
  echo "Username entered is: $USERNAME
 
- read -s USERNAME
+ 
 echo "BRO please enter your password"
  read -s PASSWORD
   echo "Password entered is: $PASSWORD
