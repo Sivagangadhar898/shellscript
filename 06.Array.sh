3a09244b93de3ae96f9b3704ffc71d04d38498ea
siva <desinidisivagangadhar@gmail.com>
2023-11-24 15:14:27 +0530

    we got it

--- 06.Array.sh
+++ 06.Array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PERSONS=("Siva" "Pavithra" "Gangadhar")
 echo "first person: ${PERSONS[0]}"
-"second person: ${PERSONS[1]}"
+echo "second person: ${PERSONS[1]}"
 echo "All persons: ${PERSONS[@]}"
