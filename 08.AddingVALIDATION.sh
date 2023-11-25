fcef07a0cfd97a8c51536b672c1a5027a9021557
siva <desinidisivagangadhar@gmail.com>
2023-11-25 19:37:59 +0530

    Condition for root access

---
+++ 08.AddingVALIDATION.sh
@@ -0,0 +1,25 @@
+USERID=$(id -u)
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo "Installation.....FAILURE"
+        exit 1
+    else
+        echo "Installation.....SUCCESS"
+
+}
+
+ if [ $USERID -ne 0 ]
+ then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+fi
+yum install mysql -y
+
+VALIDATE $? "Installing MYSQL"
+
+yum install postfix -y
+
+VALIDATE $? "Installing postfix"
+
