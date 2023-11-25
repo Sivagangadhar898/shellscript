2de7eb18c110524800905990280278300248fc96
siva <desinidisivagangadhar@gmail.com>
2023-11-25 19:31:22 +0530

    Condition for root access

--- 07.conditionforrootuser.sh
+++ 07.conditionforrootuser.sh
@@ -2,17 +2,6 @@
 
  USERID=$(id -u)
 
- VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2....FAILURE"
-        exit 1
-    else
-        echo "$2....SUCCESS"
-    fi
- }                      
-
-
  if [ $USERID -ne 0 ]
  then
     echo "ERROR:: Please run this script with root access"
@@ -21,26 +10,20 @@ fi
 
 yum install mysql -y
 
-VALIDATE $? "Installing MYSQL"
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
 
 yum install postfix -y
 
-VALIDATE $? "Installing POSTFIX"
-
-
-# if [ $? -ne 0 ]
-# then 
-#     echo "Installation of mysql is error"
-#     exit 1
-# else
-#     echo "Installation of mysql is success"
-# fi
-
-
-# if [ $? -ne 0 ]
-# then 
-#     echo "Installation of postfix is error"
-#     exit 1
-# else
-#     echo "Installation of postfix is success"
-# fi
+if [ $? -ne 0 ]
+then 
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "Installation of postfix is success"
+fi
