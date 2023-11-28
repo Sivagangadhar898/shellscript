ed5a03e72309ed524f5097bffb86bb7e47e23f87
siva <desinidisivagangadhar@gmail.com>
2023-11-28 12:27:55 +0530

    Adding logfiles scriptname date

--- 08.AddingVALIDATION.sh
+++ 08.AddingVALIDATION.sh
@@ -6,10 +6,10 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... $R Installation.....FAILURE $N"
+        echo -e "$2 ... $G Installation.....FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... $G Installation.....SUCCESS $N"
+        echo -e "$2 ... $R Installation.....SUCCESS $N"
         fi
 
 }
